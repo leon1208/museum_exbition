8376944e417c49337be4b67ba69dc31234e88ac8
leeon liu <leeon.liu@gmail.com>
2025-12-17 11:48:45 +0800

    调整docker compose进行编译镜像

--- docker/runbuild.sh
+++ docker/runbuild.sh
@@ -1,5 +1,10 @@
 #/bin/sh
 
-docker build --target frontend-nginx -t museum_exhibition_web:0.0.1 -f docker/Dockerfile .
+## 编译
+docker-compose -f docker/docker-compose.yaml build
 
-docker build --target backend-runtime -t museum_exhibition_app:0.0.1 -f docker/Dockerfile .
+## 编译加启动
+docker-compose -f docker/docker-compose.yaml up -d --build
+
+#docker build --target frontend-nginx -t museum_exhibition_web:0.0.1 -f docker/Dockerfile .
+#docker build --target backend-runtime -t museum_exhibition_app:0.0.1 -f docker/Dockerfile .
