06f3137e35efff9accc75cb6768c7937a67653e3
leeon <leeon.liu@gmail.com>
2025-12-31 13:41:41 +0800

    名字和细节调整

--- ruoyi-ui/src/layout/components/Sidebar/Logo.vue
+++ ruoyi-ui/src/layout/components/Sidebar/Logo.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script>
-import logoImg from '@/assets/logo/logo.png'
+import logoImg from '@/assets/logo/zh_icon_2.png'
 import variables from '@/assets/styles/variables.scss'
 
 export default {
@@ -35,7 +35,7 @@ export default {
   },
   data() {
     return {
-      title: '若依管理系统',
+      title: '展慧博物馆管理系统',
       logo: logoImg
     }
   }
