80342c3889ba9428d6fbb2b6226b7773e85e15e6
leeon <leeon.liu@gmail.com>
2026-01-06 14:18:59 +0800

    更改小程序的本地开发配置

--- exb-wx/config/index.js
+++ exb-wx/config/index.js
@@ -2,17 +2,26 @@ import dev from './env.dev'
 import test from './env.test'
 import prod from './env.prod'
 
+// 条件导入本地开发配置 - 使用 require 来实现动态导入
+let dev_local
+try {
+  dev_local = require('./env.local.dev').default
+} catch (e) {
+  dev_local = null
+}
+
 const envVersion = __wxConfig.envVersion
 // develop | trial | release
 // 开发 | 体验 | 正式
 
 let config = dev
 
-if (envVersion === 'trial') {
+// 如果是开发环境且本地开发配置存在，则使用本地开发配置
+if (envVersion === 'develop' && dev_local) {
+  config = dev_local
+} else if (envVersion === 'trial') {
   config = test
-}
-
-if (envVersion === 'release') {
+} else if (envVersion === 'release') {
   config = prod
 }
 
